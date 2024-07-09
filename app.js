5efc6e872562ba56ccfbf876cd9a452389ec7a49
Hammadrashid324 <hammadrashid324@gmail.com>
2024-07-08 23:55:54 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const question =[
+{
+"que":"wich of the followingis a markup language?"
+'a':"Html"
+'b':"CSS"
+'c':"Java script"
+'d':"PHP"
+"correct":"a"
+
+},
+    {
+        "que":"What year was Java script was launched?"
+        "a":"1996"
+        "b":"1995"
+        "c":"1994"
+        "correct":"b"
+        
+        },
+        {
+            "que":"what does css stands for?"
+            'a':"hyper text markup language"
+            'b':"Cascading Style Sheet"
+            'c':"Jason object notatation"
+            'd':"Helicopters terminals motorboats lamborginis"
+            "correct":"a"
+            }
+]
+let index=0:
+const quesBox=document.getElementById(quesBox)
+const loadquestion =()={
+const data=question[index]
+
+
+// console.log(data)
+}
+loadquestion ():
